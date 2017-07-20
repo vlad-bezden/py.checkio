87050cd7700ad09af3c9b89fd547edc8e3f0175b
Vlad Bezden <vlad.bezden@gmail.com>
2017-07-19 20:29:23 -0400

    text formatting

--- home/monkey_typing.py
+++ home/monkey_typing.py
@@ -29,16 +29,17 @@ all(3 ≤ len(w) and w.islower() and w.isalpha for w in words)
 
 
 def count_words(text, words):
+
     return 0
 
 
 if __name__ == '__main__':
     # These 'asserts' using only for self-checking and not necessary for
     # auto-testing
-    assert count_words("How aresjfhdskfhskd you?",
-                       {"how", "are", "you", "hello"}) == 3, "Example"
-    assert count_words("Bananas, give me bananas!!!",
-                       {"banana", "bananas"}) == 2, "BANANAS!"
+    assert count_words('How aresjfhdskfhskd you?',
+                       {'how', 'are', 'you', 'hello'}) == 3, 'Example'
+    assert count_words('Bananas, give me bananas!!!',
+                       {'banana', 'bananas'}) == 2, 'BANANAS!'
     assert count_words(
-        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
-        {"sum", "hamlet", "infinity", "anything"}) == 1, "Weird text"
+        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
+        {'sum', 'hamlet', 'infinity', 'anything'}) == 1, 'Weird text'
