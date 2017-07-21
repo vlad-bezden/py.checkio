dd6f7d65eb446c093f1898c9de82e23a1c4a7b9c
Vlad Bezden <vlad.bezden@gmail.com>
2017-07-21 12:01:33 -0400

    first implementation

---
+++ sendgrid/welcome_email.py
@@ -0,0 +1,37 @@
+import sys
+import os
+
+import sendgrid
+from sendgrid.helpers.mail import Email,  Mail, Content
+
+API_KEY = sys.argv[1]
+SUBJECT = 'Welcome'
+BODY = 'Hi {}'
+
+sg = sendgrid.SendGridAPIClient(apikey=API_KEY)
+
+
+def send_email(email, name):
+    from_email = Email('vladbezden@yahoo.com')
+    to_email = Email(email)
+    subject = SUBJECT
+    content = Content('text/plain', BODY.format(name))
+    mail = Mail(from_email, subject, to_email, content)
+    response = sg.client.mail.send.post(request_body=mail.get())
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
+
+
+def main():
+    try:
+        send_email('vladbezden@gmail.com', 'Vlad')
+    except Exception as ex:
+        print(ex)
+        os.sys.exit(1)
+
+
+if __name__ == '__main__':
+    main()
+    print('Done')
+    os.sys.exit(0)
