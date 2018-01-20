8adf7a52ab025a32b3a63267296ebdea8bccd298
Vlad Bezden <vlad.bezden@gmail.com>
2018-01-20 17:04:18 -0500

    initial assignment

---
+++ home/Long_repeat.py
@@ -0,0 +1,29 @@
+'''
+This mission is the first one of the series.
+Here you should find the length of the longest substring that consists
+of the same letter. For example, line 'aaabbcaaaa'
+contains four substrings with the same letters 'aaa', 'bb','c' and 'aaaa'.
+The last substring is the longest one which makes it an answer.
+
+Input: String.
+
+Output: Int.
+'''
+
+
+def long_repeat(line):
+    '''
+        length the longest substring that consists of the same char
+    '''
+
+    return 0
+
+
+if __name__ == '__main__':
+    # These 'asserts' using only for self-checking and not necessary
+    # for auto-testing
+
+    assert long_repeat('sdsffffse') == 4, 'First'
+    assert long_repeat('ddvvrwwwrggg') == 3, 'Second'
+
+    print('Run is good. How is Check?')
