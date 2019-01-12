348f18e0654a36bb5eae42449483e7d705bb9373
Vlad Bezden <vlad.bezden@gmail.com>
2019-01-12 15:07:58 -0500

    closed #1

---
+++ open_labyrinth.py
@@ -0,0 +1,153 @@
+"""
+The labyrinth has no walls, but bushes surround the path on each side.
+If a players move into a bush, they lose.
+The labyrinth is presented as a matrix (a list of lists): 1 is a bush and 0 is part of the path.
+The labyrinth's size is 12 x 12 and the outer cells are also bushes.
+Players start at cell (1,1).
+The exit is at cell (10,10).
+You need to find a route through the labyrinth.
+Players can move in only four directions--
+South (down [1,0]),
+North (up [-1,0]),
+East (right [0,1]),
+West (left [0, -1]).
+The route is described as a string consisting of different characters:
+"S"=South, "N"=North, "E"=East, and "W"=West.
+
+Input: A labyrinth map as a list of lists with 1 and 0.
+
+Output: The route as a string that contains "W", "E", "N" and "S".
+
+Precondition: Outer cells are pits.
+len(labyrinth) == 12
+all(len(row) == 12 for row in labyrinth)
+"""
+
+from typing import List, Dict, Tuple
+from collections import deque
+from heapq import heappop, heappush
+
+
+Maze = List[List[int]]
+Graph = Dict[str, Tuple[int, int]]
+Point = Tuple[int, int]
+
+START = (1, 1)
+
+
+def goal(maze: Maze) -> Point:
+    return (len(maze) - 2, len(maze[0]) - 2)
+
+
+def maze2graph(maze: Maze) -> Graph:
+    """Converts 2D Array to Graph: Dictionary of nodes and path"""
+
+    height = len(maze)
+    width = len(maze[0]) if height else 0
+    graph = {(i, j): [] for j in range(width) for i in range(height) if not maze[i][j]}
+
+    for row, col in graph.keys():
+        if row < height - 1 and not maze[row + 1][col]:
+            graph[(row, col)].append(("S", (row + 1, col)))
+            graph[(row + 1, col)].append(("N", (row, col)))
+        if col < width - 1 and not maze[row][col + 1]:
+            graph[(row, col)].append(("E", (row, col + 1)))
+            graph[(row, col + 1)].append(("W", (row, col)))
+
+    return graph
+
+
+def find_path_bfs(graph: Graph, goal: Point) -> str:
+    """Using Breadth First Search (BFS)"""
+
+    queue = deque([("", START)])
+    visited = set()
+
+    while queue:
+        path, current = queue.popleft()
+        if current == goal:
+            return path
+        if current in visited:
+            continue
+        visited.add(current)
+        for direction, neighbour in graph[current]:
+            queue.append((path + direction, neighbour))
+
+    return "NO WAY!"
+
+
+def find_path_dfs(graph: Graph, goal: Point) -> str:
+    """Using Depth First Search (DFS)"""
+
+    stack = deque([("", START)])
+    visited = set()
+
+    while stack:
+        path, current = stack.pop()
+        if current == goal:
+            return path
+        if current in visited:
+            continue
+        visited.add(current)
+        for direction, neighbour in graph[current]:
+            stack.append((path + direction, neighbour))
+
+    return "NO WAY!"
+
+
+def find_path_astart(graph: Graph, goal: Point) -> str:
+    """Using A* Search. It's extension of Dijkstra's algorithm"""
+
+    def heuristic(cell):
+        return abs(cell[0] - goal[0]) + abs(cell[1] - goal[1])
+
+    pr_queue = []
+    heappush(pr_queue, (0 + heuristic(START), 0, "", START))
+    visited = set()
+
+    while pr_queue:
+        _, cost, path, current = heappop(pr_queue)
+        if current == goal:
+            return path
+        if current in visited:
+            continue
+        visited.add(current)
+        for direction, neighbour in graph[current]:
+            heappush(
+                pr_queue,
+                (cost + heuristic(neighbour), cost + 1, path + direction, neighbour),
+            )
+
+    return "NO WAY!"
+
+
+def checkio(maze: Maze) -> str:
+    return find_path_bfs(maze)
+
+
+def test(maze: Maze) -> str:
+    graph = maze2graph(maze)
+    end_point = goal(maze)
+
+    funcs = [find_path_bfs, find_path_dfs, find_path_astart]
+    for f in funcs:
+        print(f"{f.__name__}: {f(graph, end_point)}")
+
+
+if __name__ == "__main__":
+    test(
+        [
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1],
+            [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1],
+            [1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 1],
+            [1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1],
+            [1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1],
+            [1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1],
+            [1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        ]
+    )
