44849a63e294cddc96320d5796ad5e51450068e8
Vlad Bezden <vlad.bezden@gmail.com>
2019-02-14 17:42:25 -0500

    added algorithm for O(n) finding largest rectangle

--- electronic_station/largest_rectangle.py
+++ electronic_station/largest_rectangle.py
@@ -1,7 +1,8 @@
 """
 Largest Rectangle in a Histogram
 
-You have a histogram. Try to find size of the biggest rectangle you can build out of the histogram bars.
+You have a histogram. Try to find size of the biggest rectangle you can build out
+of the histogram bars.
 
 Input: List of all rectangles heights in histogram
 
@@ -13,21 +14,39 @@ Precondition:
 from typing import List
 
 
+def largest_histogram_slow(histogram: List[int]) -> int:
+    """It uses O(n^2)."""
+    max_area = 0
+
+    for i in range(len(histogram)):
+        min_height = histogram[i]
+        for j in range(i, len(histogram)):
+            min_height = min(min_height, histogram[j])
+            max_area = max(max_area, min_height * ((j - i) + 1))
+    return max_area
+
+
 def largest_histogram(histogram: List[int]) -> int:
-    max_value = 0
-    for i in range(max(histogram), 0, -1):
-        adjacent_coutner = 0
-        counter = 0
-        for v in [i <= x for x in histogram]:
-            if v:
-                counter += 1
-                adjacent_coutner = max(counter, adjacent_coutner)
-            else:
-                counter = 0
-        max_value = max(max_value, adjacent_coutner * i)
+    """It uses O(n)."""
+    max_area = 0
+    stack = []
+    top = lambda: stack[-1]
 
-    return max_value
+    for i in range(len(histogram)):
+        # we are saving indexes in stack that is why we comparing last element in stack
+        # with current height to check if last element in stack not bigger then
+        # current element
+        while stack and histogram[i] <= histogram[top()]:
+            area = histogram[stack.pop()] * ((i - top() - 1) if stack else i)
+            max_area = max(max_area, area)
+        stack.append(i)
 
+    # we went through all elements of histogram
+    # check if we have something left in stack
+    while stack:
+        area = histogram[stack.pop()] * ((i - top() - 1) if stack else len(histogram))
+        max_area = max(max_area, area)
+    return max_area
 
 if __name__ == "__main__":
     assert largest_histogram([5]) == 5, "one is always the biggest"
@@ -35,4 +54,5 @@ if __name__ == "__main__":
     assert largest_histogram([1, 1, 4, 1]) == 4, "vertical"
     assert largest_histogram([1, 1, 3, 1]) == 4, "horizontal"
     assert largest_histogram([2, 1, 4, 5, 1, 3, 3]) == 8, "complex"
+    assert largest_histogram([3, 1, 6, 3, 5, 0, 8]) == 9, "Google"
     print("Done! Go check it!")
