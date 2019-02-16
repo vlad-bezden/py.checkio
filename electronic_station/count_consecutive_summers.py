de6e414b3f6d1e95196d2a191a469c19ce4dd4b1
Vlad Bezden <vlad.bezden@gmail.com>
2019-02-16 15:34:11 -0500

    added slow and fast function

--- electronic_station/count_consecutive_summers.py
+++ electronic_station/count_consecutive_summers.py
@@ -17,28 +17,44 @@ Input: Int.
 Output: Int.
 
 Precondition: Input is always a positive integer.
+
+Performance:
+count_consecutive_summers_slow took: 0.114300
+count_consecutive_summers_fast took: 0.006804
 """
 
-import math
+from itertools import count
+from timeit import timeit
 
 
-def count_consecutive_summers(num: int) -> int:
-    # TODO:
-    if num <= 2:
-        return 1
-    counter = []
-    for i in range(1, math.ceil(math.sqrt(num)) + 2):
-        middle = int(math.floor(num / i))
-        end = middle + i // 2 + 1
+def count_consecutive_summers_fast(num: int) -> int:
+    counter = 0
+    for i in count(1):
+        # find middle + half spread = end
+        end = (num // i) + (i // 2 + 1)
         start = end - i
+        if start <= 0:
+            break
         if sum(range(start, end)) == num:
-            counter.append((i, start, end))
+            counter += 1
 
-    return len(counter)
+    return counter
+
+
+def count_consecutive_summers_slow(num: int) -> int:
+    return sum(not num % i for i in range(1, num + 1, 2))
 
 
 if __name__ == "__main__":
-    assert count_consecutive_summers(42) == 4
-    assert count_consecutive_summers(99) == 6
-    assert count_consecutive_summers(1) == 1
+    for f in [count_consecutive_summers_slow, count_consecutive_summers_fast]:
+        assert f(42) == 4
+        assert f(99) == 6
+        assert f(105) == 8
+        assert f(1) == 1
+        assert f(2) == 1
+
+        num = 10000
+        t = timeit(stmt=f"f({num})", number=100, globals=globals())
+        print(f"{f.__name__} took: {t:.6f}")
+
     print("DONE")
