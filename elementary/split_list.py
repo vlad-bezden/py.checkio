23dc7a89c0d6dbc34861d63e3363afd0b0f8e7ef
Vlad Bezden <vlad.bezden@gmail.com>
2020-05-17 16:41:48 -0400

    :package: NEW: Add Split List mission

---
+++ elementary/split_list.py
@@ -0,0 +1,36 @@
+"""Split List
+    https://py.checkio.org/en/mission/split-list/
+
+
+    You have to split a given array into two arrays.
+    If it has an odd amount of elements,
+    then the first array should have more elements.
+    If it has no elements, then two empty arrays should be returned.
+
+    example
+
+    Input: Array.
+
+    Output: Array or two arrays.
+
+    Example:
+
+    split_list([1, 2, 3, 4, 5, 6]) == [[1, 2, 3], [4, 5, 6]]
+    split_list([1, 2, 3]) == [[1, 2], [3]]
+
+"""
+
+
+def split_list(items: list) -> list:
+    i = (len(items) + 1) // 2
+    return [items[:i], items[i:]]
+
+
+if __name__ == "__main__":
+    assert split_list([1, 2, 3, 4, 5, 6]) == [[1, 2, 3], [4, 5, 6]]
+    assert split_list([1, 2, 3]) == [[1, 2], [3]]
+    assert split_list([1, 2, 3, 4, 5]) == [[1, 2, 3], [4, 5]]
+    assert split_list([1]) == [[1], []]
+    assert split_list([]) == [[], []]
+
+    print("PASSED!!!")
