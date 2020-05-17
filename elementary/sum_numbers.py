03bc25ec4f35504f7c6d29a8f71edde58258fc5c
Vlad Bezden <vlad.bezden@gmail.com>
2020-05-17 14:16:52 -0400

    :ok_hand: IMPROVE: Add example with split function

--- elementary/sum_numbers.py
+++ elementary/sum_numbers.py
@@ -18,28 +18,36 @@
         'Petersen between 1845 and 1910 year') == 3755
     sum_numbers('5 plus 6 is') == 11
     sum_numbers('') == 0
+
+    Summary:
+        Using split twice faster than re
 """
 
 import re
 
 
-def sum_numbers(text: str) -> int:
+def sum_numbers_re(text: str) -> int:
     return sum(int(x) for x in re.findall(r"\b\d+\b", text))
 
 
+def sum_numbers_split(text: str) -> int:
+    return sum(int(word) for word in text.split() if word.isdigit())
+
+
 if __name__ == "__main__":
-    assert sum_numbers("hi") == 0
-    assert sum_numbers("who is 1st here") == 0
-    assert sum_numbers("my numbers is 2") == 2
-    assert (
-        sum_numbers(
-            "This picture is an oil on canvas "
-            "painting by Danish artist Anna "
-            "Petersen between 1845 and 1910 year"
+    for f in [sum_numbers_re, sum_numbers_split]:
+        assert f("hi") == 0
+        assert f("who is 1st here") == 0
+        assert f("my numbers is 2") == 2
+        assert (
+            f(
+                "This picture is an oil on canvas "
+                "painting by Danish artist Anna "
+                "Petersen between 1845 and 1910 year"
+            )
+            == 3755
         )
-        == 3755
-    )
-    assert sum_numbers("5 plus 6 is") == 11
-    assert sum_numbers("") == 0
+        assert f("5 plus 6 is") == 11
+        assert f("") == 0
 
     print("PASSED!!!")
