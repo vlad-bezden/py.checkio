cce44238e05bea6e92bc494f3deb8776c1bf5d33
Vlad Bezden <vlad.bezden@gmail.com>
2020-08-08 10:57:40 -0400

    Add another solution using sorted and timeit

--- oreilly/median_of_three.py
+++ oreilly/median_of_three.py
@@ -2,26 +2,72 @@
 
     https://py.checkio.org/en/mission/median-of-three/
 
-    Given an iterable of ints, create and return a new iterable
+    Given an List of ints, create and return a new List
     whose first two elements are the same as in items,
     after which each element equals the median of the three elements
     in the original list ending in that position.
 
-    Input: Iterable of ints.
-    Output: Iterable of ints.
+    Input: List of ints.
+    Output: List of ints.
+
+    Output:
+    median_three_sorted([1, 2, 3, 4, 5, 6, 7]) => 0.000359
+    median_three_zip([1, 2, 3, 4, 5, 6, 7]) => 0.000379
+
+    median_three_sorted([1]) => 0.000080
+    median_three_zip([1]) => 0.000119
+
+    median_three_sorted([]) => 0.000060
+    median_three_zip([]) => 0.000105
+
+    Conclusion:
+    Using sorted works faster when there are less items in the list. With more items
+    both sorted and zip have almost the same time.
 """
 
 from statistics import median
-from typing import Iterable
+from typing import Callable, List, Sequence
+from dataclasses import dataclass
+from timeit import repeat
 
 
-def median_three(items: Iterable[int]) -> Iterable[int]:
+@dataclass
+class Test:
+    data: List[int]
+    expected: List[int]
+
+
+TESTS = [
+    Test([1, 2, 3, 4, 5, 6, 7], [1, 2, 2, 3, 4, 5, 6]),
+    Test([1], [1]),
+    Test([], []),
+]
+
+
+def median_three_sorted(items: List[int]) -> List[int]:
+    return [
+        v if i < 2 else sorted(items[i - 2 : i + 1])[1]
+        for i, v in enumerate(items)
+    ]
+
+
+def median_three_zip(items: List[int]) -> List[int]:
     return items[:2] + [*map(median, zip(items, items[1:], items[2:]))]
 
 
-if __name__ == "__main__":
-    assert list(median_three([1, 2, 3, 4, 5, 6, 7])) == [1, 2, 2, 3, 4, 5, 6]
-    assert list(median_three([1])) == [1]
-    assert list(median_three([])) == []
-
+def validate(funcs: Sequence[Callable[[List[int]], List[int]]]) -> None:
+    for test in TESTS:
+        for f in funcs:
+            result = f(test.data)
+            assert result == test.expected, f"{f.__name__}({test}), {result = }"
     print("PASSED!!!")
+
+
+if __name__ == "__main__":
+    funcs = [median_three_sorted, median_three_zip]
+    validate(funcs)
+    for test in TESTS:
+        for f in funcs:
+            t = repeat(stmt=f"f({test.data})", repeat=3, number=100, globals=globals())
+            print(f"{f.__name__}({test.data}) => {min(t):.6f}")
+        print()
